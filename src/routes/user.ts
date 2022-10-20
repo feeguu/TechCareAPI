129fa5c32afc07a313441cc1624b97635eddea11
feeguu <gusiqueira2010@gmail.com>
2022-10-20 11:31:40 -0300

    refactor: remove unnecessary if clauses

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,48 +3,59 @@ import { verify } from "argon2"
 import { PrismaClient } from "@prisma/client"
 import { generateToken, getUserId } from "../utils/token"
 
+type RequestBody = {
+	username: string
+	password: string
+}
+
 const prisma = new PrismaClient()
 
 const userRoutes = express.Router()
 
 userRoutes.post("/auth", async (req, res) => {
-    const { username, password } = req.body
+	const { username, password } = req.body as RequestBody
 
-    if (!username || !password) {
-        return res.status(400).json({ error: "Missing params" })
-    }
+	if (!username || !password) {
+		return res.status(400).json({ error: "Missing params" })
+	}
 
-    const user = await prisma.user.findUnique({ where: { username } })
+	const user = await prisma.user.findUnique({ where: { username } })
 
-    if (!user) {
-        return res.status(401).json({ error: "Wrong username or password" })
-    }
+	if (user && (await verify(user.password, password))) {
+		return res.status(200).json({ token: generateToken(user.id) })
+	}
 
-    if (!(await verify(user.password, password))) {
-        return res.status(401).json({ error: "Wrong username or password" })
-    }
-
-    return res.status(200).json({ token: generateToken(user.id) })
+	return res.status(401).json({ error: "Wrong username or password" })
 })
 
 userRoutes.get("/auth", async (req, res) => {
-    const token = req.header("Authorization")?.split(" ")[1]
-    if (!token) {
-        return res.status(401).json({ authenticated: false })
-    }
-    const userId = getUserId(token) // Already checks if token is valid.
+	const token = req.header("Authorization")?.split(" ")[1]
+	if (!token) {
+		return res.status(401).json({ authenticated: false })
+	}
+	const userId = getUserId(token) // Already checks if token is valid.
 
-    if (!userId) {
-        return res.status(401).json({ authenticated: false })
-    }
+	if (!userId) {
+		return res.status(401).json({ authenticated: false })
+	}
 
-    const user = await prisma.user.findUnique({ where: { id: userId } })
+	const user = await prisma.user.findUnique({
+		where: { id: userId },
+		select: {
+			id: true,
+			role: true,
+			username: true,
+			name: true,
+			birthdate: true,
+			contact: true,
+		},
+	})
 
-    if (!user) {
-        return res.status(401).json({ authenticated: false })
-    }
+	if (!user) {
+		return res.status(401).json({ authenticated: false })
+	}
 
-    return res.status(200).json({ authenticated: true, user })
+	return res.status(200).json({ authenticated: true, user })
 })
 
 export default userRoutes
