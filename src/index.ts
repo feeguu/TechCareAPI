a6deb7163e8122eeffe5cf7589cd69f287090801
Gustavo Santos Siqueira <gusiqueira2010@gmail.com>
2022-10-22 00:16:21 -0300

    feat: add http custom error class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import caregiverRoutes from "./routes/caregiver"
 import userRoutes from "./routes/user"
+import { HttpError } from "./utils/httpError"
 
 const app = express()
 
 app.use(express.json())
+
+app.use((err: Error, req: Request, res: Response) => {
+	if (err instanceof HttpError) {
+		return res.status(err.code).json({ error: err.message })
+	}
+	return res.status(500).json({ error: err.message })
+})
 
 app.use("/user", userRoutes)
 app.use("/caregiver", caregiverRoutes)
