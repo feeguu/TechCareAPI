c98f4a0384946b08cf8d3b017348295881bf7965
Gustavo Santos Siqueira <gusiqueira2010@gmail.com>
2022-10-23 16:36:26 -0300

    fix: checks if user from token exists

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -1,15 +1,19 @@
+import { PrismaClient } from "@prisma/client"
 import { NextFunction, Request, Response } from "express"
 import unautheticatedError from "../errors/unautheticatedError"
-import { validateToken } from "../utils/token"
+import { getUserId, validateToken } from "../utils/token"
 
-export default function isAuth(req: Request, res: Response, next: NextFunction) {
+const prisma = new PrismaClient()
+
+export default async function isAuth(req: Request, res: Response, next: NextFunction) {
 	try {
 		const token = req.headers.authorization?.split(" ")[1]
-
 		if (token && validateToken(token)) {
-			res.locals.token = token
-			return next()
-		}
+			if (await prisma.user.findUnique({ where: { id: getUserId(token) } })) {
+				res.locals.token = token
+				return next()
+			
+		}}
 		throw unautheticatedError
 	} catch (e) {
 		next(e)
