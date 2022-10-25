ba68dc83dead0ffc3cf24e4686ce4673539bd899
Gustavo Santos Siqueira <gusiqueira2010@gmail.com>
2022-10-25 18:21:17 -0300

    feat: add get all patients route

--- src/routes/patients.ts
+++ src/routes/patients.ts
@@ -1,8 +1,23 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, Severity } from "@prisma/client"
+import dayjs from "dayjs"
+import customParserFormat from "dayjs/plugin/customParseFormat"
 import express from "express"
+import missingParamsError from "../errors/missingParamsError"
+import isAdmin from "../middlewares/isAdmin"
 import isAuth from "../middlewares/isAuth"
-import getRole from "../utils/getRole"
-import { getUserId } from "../utils/token"
+
+type PatientRequestBody = {
+	name: string
+	birthdate: string
+	severity: Severity
+	bloodType: string
+	allergies: string
+	contact: string
+	height: number
+	medicines: string
+	weaknesses: string
+	weight: number
+}
 
 const prisma = new PrismaClient()
 
@@ -10,25 +25,12 @@ const patientsRoutes = express.Router()
 
 patientsRoutes.get("/", isAuth, async (req, res, next) => {
 	try {
-        const userId = getUserId(res.locals.token)
-        const role = await getRole(userId)
-        if (role === "CAREGIVER") {
-            const patients = await prisma.patient.findMany({ where: { care: { some: { caregiverId: userId } } } })
-            return res.status(200).json(patients)
-        }
-        if (role === "ADMIN") {
 		const patients = await prisma.patient.findMany()
 		return res.status(200).json(patients)
 	} catch (e) {
 		next(e)
 	}
 })
-
-    } catch (e) {
-        next(e)
-    }
-})
-
 
 
 export default patientsRoutes
