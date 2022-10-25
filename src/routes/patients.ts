528c6bd80a48ae80bc313d9e2ee2a27999a43e87
Gustavo Santos Siqueira <gusiqueira2010@gmail.com>
2022-10-25 20:16:11 -0300

    feat: get unique patient route

--- src/routes/patients.ts
+++ src/routes/patients.ts
@@ -2,6 +2,7 @@ import { PrismaClient, Severity } from "@prisma/client"
 import dayjs from "dayjs"
 import customParserFormat from "dayjs/plugin/customParseFormat"
 import express from "express"
+import { HttpError } from "../errors/HttpError"
 import missingParamsError from "../errors/missingParamsError"
 import isAdmin from "../middlewares/isAdmin"
 import isAuth from "../middlewares/isAuth"
@@ -77,6 +78,27 @@ patientsRoutes.post("/", isAuth, isAdmin, async (req, res, next) => {
 			},
 		})
 		return res.status(200).json(patient)
+	} catch (e) {
+		next(e)
+	}
+})
+
+patientsRoutes.get("/:patientId", isAuth, async (req, res, next) => {
+	try {
+		const { patientId } = req.params as { patientId: string }
+		if (res.locals.role === "ADMIN") {
+			const patient = await prisma.patient.findUnique({ where: { id: patientId } })
+			if (!patient) throw new HttpError(400, "Patient not found.")
+			return res.status(200).send(patient)
+		}
+		if (res.locals.role === "CAREGIVER") {
+			const patients = await prisma.patient.findMany({
+				where: { id: patientId, care: { some: { caregiverId: res.locals.id } } },
+				take: 1,
+			})	
+			if (patients.length === 0) throw new HttpError(400, "Patient not found.")
+			return res.status(200).send(patients[0])
+		}
 	} catch (e) {
 		next(e)
 	}
